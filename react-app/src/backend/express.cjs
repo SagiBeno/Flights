38898a0a24a4454b3471d089db471ada57883113
MrMancs <marcell.szatmari2007@gmail.com>
2025-10-20 09:10:14 +0200

    Begin fill backend with data

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -7,6 +7,19 @@ app.use(cors())
 
 const port = 3333
 
+const cities = [
+  { name: "Paris", country: "France", image: "TODO-add-static-img-url-paris.jpg" },
+  { name: "Tokyo", country: "Japan", image: "TODO-add-static-img-url-tokyo.jpg" },
+  { name: "New York", country: "USA", image: "TODO-add-static-img-url-newyork.jpg" },
+  { name: "Lipseszentadorján", country: "GET", image: "cities-from-backend.jpg" },
+  { name: "Kazincbarcika", country: "GET", image: "cities-from-backend.jpg" },
+  { name: "Iklódbördőce", country: "GET", image: "cities-from-backend.jpg" },
+];
+
+app.post("/destinations",(req, res) => {
+    res.status(200).json()
+})
+
 app.listen(port, () => {
     console.log("Szerver mükszik itt: " + port)
 })
