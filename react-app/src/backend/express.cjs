00e6fee3bc5260b959fda3b216c0134604fc6542
MrMancs <marcell.szatmari2007@gmail.com>
2025-10-21 18:40:16 +0200

    Succesfull login with database data

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -8,10 +8,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static(path.join(process.cwd(), "public")))
 
-const users = [ //TODO adatbázis kéne ide sztem
-    { username: 'JohnDoe', email: 'JohnDoe@example.com' , password: '12345678' }
-]
-
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -27,21 +23,20 @@ app.post("/login", (req, res) => {
         if(connectError) console.warn(connectError)
         else {
             //console.log("Sikeres connect")
-            conn.query(`SELECT username, email, password FROM accounts WHERE email="${email}" AND password="${password}"`),
+            conn.query(`SELECT username, email, password FROM accounts WHERE email="${email}" AND password="${password}"`,
                 (err, result, fields) => {
-                    console.log("Result: ", result)
-                    if(err) console.warn(err)
+                    if(err) console.log(err)
                     else if (result) {
-                        users = [...result]
-                        console.log("users", users)
+                        const users = [...result]
+                        
 
                         if (users.length < 1) res.status(300).json({login: false})
                         else {
-                            console.log(result)
+                            res.status(200).json({login: true, username: result.username})
                         }
                         
                     }
-                }
+                })
         }
     })
 
@@ -59,27 +54,12 @@ app.post("/login", (req, res) => {
 
 const port = 3333
 
-const cities = [
-  { name: "Paris", country: "France", image: "/paris.jpg" },
-  { name: "Tokyo", country: "Japan", image: "/tokyo.jpg" },
-  { name: "New York", country: "USA", image: "/newyork.jpg" },
-  { name: "Lipseszentadorján", country: "Hungary", image: "/lipseszentadorjan.jpg" },
-  { name: "Kazincbarcika", country: "Hungary", image: "/kazincbarcika.jpg" },
-  { name: "Iklódbördőce", country: "Hungary", image: "/iklodbordoce.jpg" },
-];
-
-const flights = [
-  { from: "Paris", to: "Tokyo", flight: "AF274", depart: "10:30", arrive: "04:45", price: "€850" },
-  { from: "New York", to: "Rome", flight: "DL198", depart: "13:00", arrive: "02:15", price: "$750" },
-  { from: "TODO", to: "GET", flight: "fromBackend", depart: "12:34", arrive: "21:09", price: "$1" },
-];
-
 app.get("/destinations",(req, res) => {
-    res.status(200).json( { cities } )
+
 })
 
 app.get('/flight-info', (req, res) => {
-    res.status(200).json({ flights })
+
 })
 
 app.listen(port, () => {
