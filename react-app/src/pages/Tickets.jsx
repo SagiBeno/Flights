bc8022029b0eb9c3bfe32be7d6511efe40bb9133
hajos8 <rabb.andras@gmail.com>
2025-10-21 11:42:16 +0200

    Fixed tickets

--- react-app/src/pages/Tickets.jsx
+++ react-app/src/pages/Tickets.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
+import TicketForm from "../components/TicketForm.jsx";
+
 export default function Tickets() {
   const [basket, setBasket] = useState([]);
 
   const handleSubmit = e => {
     e.preventDefault();
-    setBasket([...basket, form]);
+    console.log(e.target);
+    //setBasket([...basket, form]);
   };
 
   return (
     <div className="container">
-      {/*<TicketForm onSubmit={handleSubmit} />*/}
+      <TicketForm onSubmit={handleSubmit} />
 
       <h4 className="mt-4">Your Basket</h4>
       <ListGroup>
